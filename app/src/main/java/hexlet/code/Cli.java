98f0dc3624165ecd1f1abb7cc2d453a584dbd75b
SmyslovSV <smyslovsv@yandex.ru>
2023-07-24 23:44:45 +0300

    step03 create class Cli.java, call greeting() from App. create Makefile

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
+}
