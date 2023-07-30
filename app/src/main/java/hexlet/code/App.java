3170bc2ee91670008fc670c1aaa41d6584965455
SmyslovSV <smyslovsv@yandex.ru>
2023-07-30 22:34:35 +0300

    step10 add gradlew

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.Cli;
+import hexlet.code.games.Progression;
+import hexlet.code.games.Gcd;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Prime;
+import hexlet.code.games.Even;
 
 import java.util.Scanner;
 
